dc4c454bc2d69f3bb77baa6f37ad2082752d1808
Alexandra Franzén <156119504+Xikitita@users.noreply.github.com>
2024-05-22 14:40:09 +0200

    Main (#2)
    * added instructions
    
    * Added endpoints for backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,14 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import User from "./models/User"
+import User from "./models/User";
+import expressListEndpoints from "express-list-endpoints";
+import dotenv from "dotenv";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+//.env
+dotenv.config()
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
@@ -18,10 +23,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Route handler
+app.get("/", (req, res) => {
+  const endpoints = expressListEndpoints(app)
+  res.json(endpoints)
+})
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("This is the backend!");
 });
 
 //Registration endpoint, to create a new user.
@@ -31,7 +41,7 @@ app.post("/sign-up", async (req, res) => {
     const user = new User({
       name,
       email,
-      password: bcrypt.hashSync(password, 10),
+      password: bcrypt.hashSync(password),
   });
     await user.save();
     res.status(201).json({ id: user._id, accessToken: user.accessToken });
@@ -41,6 +51,9 @@ app.post("/sign-up", async (req, res) => {
       .json({ message: "Could not create user", error: err.message });
   }
 });
+
+
+//Middlewares
 
 //An authenticated endpoint which only returns content if the `Authorization` header with the user's token was correct.
 
@@ -54,7 +67,18 @@ const authenticateUser = async (req, res, next) => {
   }
 }
 
+
 app.get("sign-in", authenticateUser)
+app.get("/sign-in", async (req, res) => {
+  const user = await User.findOne({name: req.body.name});
+  
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({userId: user._id, accessToken: user.accessToken});
+  } else {
+    res.json({notFound: true})
+  }
+
+
 
 app.get("/sign-in", async (req, res) => {
   res.json({ secret: "You are successfully signed in" });
