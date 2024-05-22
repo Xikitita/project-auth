13ac7bb9b1f451ddcf580bf3294bacf430a2c5f4
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-05-22 14:33:33 +0200

    Added endpoints for backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,14 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import User from "./models/User"
+import User from "./models/User";
+import expressListEndpoints from "express-list-endpoints";
+import dotenv from "dotenv";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+//.env
+dotenv.config()
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
@@ -18,10 +23,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Route handler
+app.get("/", (req, res) => {
+  const endpoints = expressListEndpoints(app)
+  res.json(endpoints)
+})
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("This is the backend!");
 });
 
 //Registration endpoint, to create a new user.
@@ -31,7 +41,7 @@ app.post("/sign-up", async (req, res) => {
     const user = new User({
       name,
       email,
-      password: bcrypt.hashSync(password, 10),
+      password: bcrypt.hashSync(password),
   });
     await user.save();
     res.status(201).json({ id: user._id, accessToken: user.accessToken });
@@ -42,8 +52,8 @@ app.post("/sign-up", async (req, res) => {
   }
 });
 
-//An authenticated endpoint which only returns content if the `Authorization` header with the user's token was correct.
 
+//Middlewares
 const authenticateUser = async (req, res, next) => {
   const user = await User.findOne({ accessToken: req.header("Authorization") })
   if(user) {
@@ -54,10 +64,16 @@ const authenticateUser = async (req, res, next) => {
   }
 }
 
-app.get("sign-in", authenticateUser)
 
+app.get("sign-in", authenticateUser)
 app.get("/sign-in", async (req, res) => {
-  res.json({ secret: "You are successfully signed in" });
+  const user = await User.findOne({name: req.body.name});
+  
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({userId: user._id, accessToken: user.accessToken});
+  } else {
+    res.json({notFound: true})
+  }
 });
 
 // Start the server
